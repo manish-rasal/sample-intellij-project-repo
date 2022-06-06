76dafe69ee42da39069604839aac2bcf1a70582d
manrasal <manishrasal2000@gmail.com>
2022-06-06 16:55:49 +0530

    Provided sub() method

--- src/com/sapient/asde/ui/CalculatorClass.java
+++ src/com/sapient/asde/ui/CalculatorClass.java
@@ -5,5 +5,6 @@ public class CalculatorClass {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("10.0+27.67= "+ calculator.add(10.0,27.67));
+        System.out.println("10.0+27.67= "+ calculator.sub(10.0,27.67));
     }
 }
